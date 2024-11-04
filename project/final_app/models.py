06c2294b00f4baac50c4d10b5aedcb58b4dd0a71
Thomas McGowan <thomas.mcgowan@westpoint.edu>
2024-11-04 11:48:30 -0500

    Django project

---
+++ project/final_app/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+# Create your models here.
+
+class Player(models.Model):
+    pass
+    '''
+    pk: (player id, year)
+    firstName
+    lastName
+    currentTeam
+    offenseStats
+    defenseStats
+    putcherStats
+    salary
+    carrer
+    leauge
+    year
+    playerID
+    '''
